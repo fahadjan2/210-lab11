5d180e644876ed9ba8a025196c5c0ed3ec04072a
fahadjan2 <fahadfawad2006@gmail.com>
2025-09-19 23:13:14 -0700

    Commit

--- code.cpp
+++ code.cpp
@@ -14,33 +14,39 @@ struct Game {
     int totalPlays;
     string * reviews;
 
-   // ~Game() {
-       // if (reviews)
-        //    delete [] reviews;
-       // reviews = nullptr;
-   // }
+   ~Game() {
+        if (reviews)
+            delete [] reviews;
+        reviews = nullptr;
+    }
 };
 
 void inputGame(Game *);
+void displayGame(Game *);
 
 int main() {
     Game * gameLibrary = new Game[NR_GAMES];
-    cout << "test" << endl;
+    cout << "test12" << endl;
 
     for (int i = 0; i < NR_GAMES; i++) {
         inputGame(&gameLibrary[i]);
+    }
+
+    for (int i = 0; i < NR_GAMES; i++) {
+        displayGame(&gameLibrary[i])
     }
 
     delete [] gameLibrary;
     return 0;
 }
 
+//inputGame() function takes in a dynamic struct array and asks user input and initializes all the members in the struct, including the dynamic reviews array. Doesn't return anything
 void inputGame(Game * gLibrary) {
     static int num = 1;
     cout << "Enter info for game " << num << endl;
     
     cout << "Title: ";
-    cin >> gLibrary->title;
+    getline(cin, gLibrary->title);
 
     cout << "Players currently playing: ";
     cin >> gLibrary->currentActive;
@@ -48,10 +54,29 @@ void inputGame(Game * gLibrary) {
     cout << "Total plays: ";
     cin >> gLibrary->totalPlays;
 
+    gLibrary -> reviews = new string[NR_REVIEWS];
     cout << "Enter " << NR_REVIEWS << " reviews: " << endl;
     for (int i = 0; i < NR_REVIEWS; i++) {
         cout << "Enter review " << i + 1 << endl;
-        cin >> gLibrary->reviews[i];
+         getline(cin, gLibrary->reviews[i]);
+    }
+
+    cout << endl;
+    num++;
+}
+
+//displayGame() function receives a value in a struct array and displays all the members in a given struct index. Doesn't return anything
+void displayGame(Game * gLibrary) {
+    static int num = 1;
+    cout << "\nGame " << num << ": " << endl;
+    cout << "Title: " << gLibrary->title << endl;
+    cout << "Current Active Players: " << gLibrary->currentActive << endl;;
+    cout << "Total Plays: " << gLibrary->totalPlays << endl;
+    
+    cout << "Reviews: " << endl;
+    for (int i = 0; i < NR_REVIEWS; i++) {
+        cout << "Review " << i + 1 << ": " << gLibrary->reviews[i] << endl;
+        cout << endl;
     }
 
     num++;
