c3406e0b40a3951e30982f9a49ea471331cf26e5
fahadjan2 <fahadfawad2006@gmail.com>
2025-09-19 22:08:46 -0700

    Commit

--- code.cpp
+++ code.cpp
@@ -1,6 +1,26 @@
+// COMSC-210 | Lab 11 | Fahad Fawad Ahmad
+// IDE used: Visual Studio
 #include <iostream>
 using namespace std;
 
+const int NR_GAMES = 3;
+
+//A struct that stores info of a game in a game library, the title, total plays, and how many are currently playing it. It also stores reviews in a dynamic array
+struct Game {
+    string title;
+    int totalPlays;
+    int currentActive;
+    string * reviews;
+
+    ~Game() {
+        if (reviews)
+            delete [] reviews;
+        reviews = nullptr;
+    }
+};
+
 int main() {
-    cout << "hello" << endl; 
+    Game * gameLibrary = new Game[NR_GAMES];
+
+    
 }
