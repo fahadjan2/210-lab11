70fc9b0776a0eabdd3d4f8850c7c3da9f6d51f24
fahadjan2 <fahadfawad2006@gmail.com>
2025-09-19 22:23:15 -0700

    Commit

--- code.cpp
+++ code.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 using namespace std;
 
-const int NR_GAMES = 3;
+const int NR_GAMES = 3; //The number of games in the library
+const int NR_REVIEWS = 4; //Number of reviews
 
-//A struct that stores info of a game in a game library, the title, total plays, and how many are currently playing it. It also stores reviews in a dynamic array
+//A struct that stores info of a game in a game library, the title, how many are currently playing it, and total plays. It also stores reviews in a dynamic array
 struct Game {
     string title;
-    int totalPlays;
     int currentActive;
+    int totalPlays;
     string * reviews;
 
     ~Game() {
@@ -19,8 +20,33 @@ struct Game {
     }
 };
 
+void inputGame(Game *);
+
 int main() {
     Game * gameLibrary = new Game[NR_GAMES];
 
+    for (int i = 0; i < NR_GAMES; i++) {
+        inputGame(&gameLibrary[i]);
+    }
+}
+
+void inputGame(Game * gLibrary) {
+    int num = 1;
+    cout << "Enter info for game " << num << endl;
     
+    cout << "Title: ";
+    cin >> gLibrary->title;
+
+    cout << "Players currently playing: ";
+    cin >> gLibrary->currentActive;
+
+    cout << "Total plays: ";
+    cin >> gLibrary->totalPlays;
+
+    cout << "Enter " << NR_REVIEWS << " reviews: " << endl;
+    for (int i = 0; i < NR_REVIEWS; i++) {
+        cout << "Enter review " << i << endl;
+        cin >> gLibrary->reviews[i];
+        cout << endl;
+    }
 }
